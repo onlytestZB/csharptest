848f7d3499de28e6295d50f245bc37882389e37e
onlytestzb <zb3215286@hotmail.com>
2018-02-08 22:57:49 +0800

    test

---
+++ XmlUsage/XmlUsage/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace XmlUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument doc = XDocument.Load(".\\test.xml");
+            
+
+            foreach(XElement item in doc.Root.Descendants("DATA"))
+            {
+                            
+                if(item.Attribute("DATANAME").Value== "TDFLMJ_Transition")
+                {
+                    foreach(XElement row in item.Descendants("ROW"))
+                    {
+                        foreach(XAttribute att in row.Attributes())
+                        {
+                            Console.Write("{0}:{1}",att.Name,att.Value);
+                        }                      
+                        Console.WriteLine();
+                        foreach(XElement dl in row.Descendants("TDFLMJ"))
+                        {
+                            foreach (XAttribute dl_att in dl.Attributes())
+                            {
+                                Console.WriteLine("{0}:{1}",dl_att.Name,dl_att.Value);
+                            }
+                            Console.WriteLine();
+                        }
+                       
+                    }
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ XmlUsage/XmlUsage/ZDCUNZU.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XmlUsage
+{
+    public class ZDCUNZU
+    {
+        public String Zheng;
+        public String Cun;
+        public List<ZhengCunZu> zu;
+        public ZDCUNZU()
+        {
+            zu = new List<ZhengCunZu>();
+        }
+        public void addRow(ZhengCunZu addzu)
+        {
+            Zheng = "abcgit test2";
+            zu.Add(addzu);
+        }
+    }
+    public class ZhengCunZu
+    {
+       
+        public String Zu;
+        public Dictionary<string, int> dlinfo;
+        public ZhengCunZu()
+        {
+            Zu = "";
+            for(int i=1;i<4;i++)
+            {
+                dlinfo.Add("01" + i,0);
+            }
+            for (int i = 1; i < 4; i++)
+            {
+                dlinfo.Add("02" + i, 0);
+            }
+            for (int i = 1; i < 4; i++)
+            {
+                dlinfo.Add("03" + i, 0);
+            }
+            for (int i = 1; i < 4; i++)
+            {
+                dlinfo.Add("04" + i, 0);
+            }
+            for (int i = 1; i < 8; i++)
+            {
+                dlinfo.Add("10" + i, 0);
+            }
+            for (int i = 1; i < 10; i++)
+            {
+                dlinfo.Add("11" + i, 0);
+            }
+            for (int i = 1; i < 8; i++)
+            {
+                dlinfo.Add("12" + i, 0);
+            }
+            for (int i = 1; i < 6; i++)
+            {
+                dlinfo.Add("20" + i, 0);
+            }
+        }
+    }    
+}
